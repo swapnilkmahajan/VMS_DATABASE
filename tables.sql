d5490b70ea901a0e7ce22a0bc3b38a6bfaae891c
rutagadgil <gadgil.r@husky.neu.edu>
2014-11-23 14:44:05 -0500

    clinic address prompt
    clinic address prompt

--- tables.sql
+++ tables.sql
@@ -87,7 +87,7 @@ primary key (bill_id, description)
 
 /* Add first Doctor*/
 insert into person_id values (1);
-insert into Doctor values (1 , '&DOC_NAME' ,'&DOC');
+insert into Doctor values (1 , '&DOC_NAME' ,'&CLINIC_ADDRESS');
 insert into authentication values (1 , '&USERNAME' ,'&PASS');
 commit;
 
